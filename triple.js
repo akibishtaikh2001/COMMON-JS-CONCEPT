98dd065039190b0f0d3649a64580f8ac45184daa
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-03 03:25:21 +0600

    double equal (==) vs triple equal (===), implicit conversion

---
+++ triple.js
@@ -0,0 +1,15 @@
+// always use ===
+// equal comparison doesnt work for non-primitive
+
+const first = [45, 54, 98];
+const second = [45, 54, 98];
+if (first === second) {
+    console.log('values are equal');
+}
+else {
+    console.log('vlaues are not equal');
+}
+
+// type coercion
+// type conversion
+// type casting
