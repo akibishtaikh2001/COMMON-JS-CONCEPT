89ad6b9b984899941b9bd8a9dce14026f4b00f9e
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-03 01:30:24 +0600

    Primitive and non-primitive data type

---
+++ types.js
@@ -0,0 +1,32 @@
+// strongly type Language
+// int a = 5;
+// string b = 'alim halim Khalim';
+// bool c = True;
+// object student = { name: 'noya daman', id: 55};
+// int[] numbers = [12, 45, 78];
+// string[] friends = ['abul', 'babul'];
+
+// JavaScript is a dynamic type Language
+
+// primitive type
+const a = 5;
+const b = 'samsu kopai na eakhon ar';
+const d = true;
+
+
+// non-primitive
+const ages = [45, 65, 48];
+const student = { id: 23, class: 7 };
+
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+let p = { job: 'web-devloper' };
+let q = p;
+q.job = 'fornt end developer';
+console.log(p, q);
